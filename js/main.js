51503f7d251298fe49ebee982b74af9ec6927d17
jek-12 <zekaoleksyuk@gmail.com>
2021-08-20 02:52:19 +0300

    scroll pixel_perfect pag false

--- js/main.js
+++ js/main.js
@@ -39,6 +39,17 @@ $.scrollify({
             });
         swap++;
     }
+
+
+        $(".dws-progress-bar").circularProgress({
+            color: "#25ffe4",
+            line_width: 17,
+            height: "350px",
+            width: "350px",
+            percent: 0,
+            // counter_clockwise: true,
+            starting_position: 25
+        }).circularProgress('animate', 100, 2000);
     },
     afterResize:function() {},
     afterRender:function() {
--- js/slider.js
+++ js/slider.js
@@ -7,7 +7,7 @@
         loop: true,
         grabCursor: true,
         pagination: {
-            el: '.swiper-pagination',
+            el: '.swiper-pagination-0',
             clickable: true,
         },
         breakpoints: {
@@ -32,7 +32,7 @@
 
     });
 
-setInterval(()=> $(window).width() > 1100 ? $(".swiper-pagination").css("display", "none"):$(".swiper-pagination").css("display", "block"),100);
+setInterval(()=> $(window).width() > 1100 ? $(".swiper-pagination-0").css("display", "none"):$(".swiper-pagination-0").css("display", "block"),100);
 
     let swiper_s = new Swiper('.team_slider', {
         slidesPerView: 4,
@@ -41,7 +41,7 @@ setInterval(()=> $(window).width() > 1100 ? $(".swiper-pagination").css("display
         loop: true,
         grabCursor: true,
         pagination: {
-            el: '.swiper-pagination',
+            el: '.swiper-pagination-1',
             clickable: true,
         },
         breakpoints: {
@@ -65,3 +65,38 @@ setInterval(()=> $(window).width() > 1100 ? $(".swiper-pagination").css("display
         }
 
     });
+    setInterval(()=> $(window).width() > 1100 ? $(".swiper-pagination-1").css("display", "none"):$(".swiper-pagination-1").css("display", "block"),100);
+
+
+    let swiper_t = new Swiper('.skills-slider', {
+        slidesPerView: 4,
+        spaceBetween: 80,
+        slidesPerGroup: 1,
+        loop: true,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination-2',
+            clickable: true,
+        },
+        breakpoints: {
+            1: {
+                slidesPerView: 1,
+                spaceBetween: 24,
+            },
+            500: {
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+            740: {
+                spaceBetween: 30,
+                slidesPerView: 3,
+
+            },
+            1100: {
+                spaceBetween: 80,
+                slidesPerView: 4,
+            }
+        }
+
+    });
+    setInterval(()=> $(window).width() > 1100 ? $(".swiper-pagination-2").css("display", "none"):$(".swiper-pagination-2").css("display", "block"),100);
